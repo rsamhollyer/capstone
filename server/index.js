102efa05bc7d697f403a7ec0d116e93d2faa9062
Sam Hollyer <rsamhollyer@gmail.com>
2021-02-05 10:14:47 -0600

    building out file structure and initlizing the index.js file

---
+++ server/index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+// Getting Express ready
+const http = require("http");
+const app = require("express")();
+const morgan = require("morgan");
+const server = http.createServer(app);
+const logger = morgan("dev");
+
+const PORT = 9999;
+const HOST = "localhost";
+
+//For Fav-Icon
+const bodyParser = require("body-parser");
+const favicon = require("serve-favicon");
+const path = require("path");
+
+//Fav-Icon
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+
+app.use(logger);
+
+app.get("/", (req, res) => {
+	res.send(`Hi Express`);
+});
+
+server.listen(POST, HOST, () => {
+	console.log(`Listening on http://${HOST}:${PORT}`);
+});
