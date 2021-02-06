e4ca8ebb37429d064e29a5fdc027f2ae0afa3be1
Sam Hollyer <rsamhollyer@gmail.com>
2021-02-06 10:29:18 -0600

    changed how the graphs domain and range was being displayed. needed to use an effect and state hook to get them to set up properly

--- weather-piece/src/Components/GraphComponent.jsx
+++ weather-piece/src/Components/GraphComponent.jsx
@@ -1,12 +1,27 @@
 import { VictoryChart, VictoryLine, VictoryTheme } from "victory";
+import { useState, useEffect } from "react";
 
 export default function GraphComponent(props) {
 	const { chartData, graphClass, graphTitle } = props;
+	const [domain, setDomain] = useState(0);
+
+	useEffect(() => {
+		if (graphTitle === `Speed Graph`) {
+			setDomain(25);
+		} else if (graphTitle === `Alt. Graph`) {
+			setDomain(3000);
+		} else {
+			setDomain(200);
+		}
+	}, []);
 
 	return (
 		<div className={`${graphClass}`}>
 			<h2>{graphTitle}</h2>
-			<VictoryChart theme={VictoryTheme.material}>
+			<VictoryChart
+				theme={VictoryTheme.material}
+				domain={{ x: [0, 2000], y: [0, `${domain}`] }}
+			>
 				<VictoryLine
 					style={{
 						data: { stroke: "#c43a31" },
